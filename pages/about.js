c765eac8c26fc85941e1a5576d10dff8143a7810
Tyrique Daniel <37724661+TyriqueDaniel@users.noreply.github.com>
2021-08-25 18:33:47 -0600

    Added images and meta tags

--- pages/about.js
+++ pages/about.js
@@ -1,30 +1,28 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import styles from '../styles/Home.module.css';
-export default function Home() {
+import Head from 'next/head'
+
+const About = () => {
   return (
     <>
       <Head>
-        <title>Ninja List | Home</title>
+        <title>Ninja List | About</title>
       </Head>
       <div>
-        <h1 className={styles.title}>HomePage</h1>
-        <p className={styles.text}>
+        <h1>About</h1>
+        <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut ipsum
           blanditiis et, quae eligendi incidunt ratione ullam iste qui error
           debitis perspiciatis sint consectetur harum sit eum, accusantium
           temporibus nemo!
         </p>
-        <p className={styles.text}>
+        <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut ipsum
           blanditiis et, quae eligendi incidunt ratione ullam iste qui error
           debitis perspiciatis sint consectetur harum sit eum, accusantium
           temporibus nemo!
         </p>
-        <Link href="ninjas">
-          <a className={styles.btn}>See Ninja Listing</a>
-        </Link>
       </div>
     </>
   );
-}
+};
+
+export default About;
